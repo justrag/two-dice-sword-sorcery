a5f6ef0f74b75568b0909276f42071d30c1deed9
Just RAG <justrag@gmail.com>
2016-09-19 14:35:00 +0200

    attack arrows

--- src/containers/FigureRow.jsx
+++ src/containers/FigureRow.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import UpArrow from 'material-ui/svg-icons/action/open-with';
 import Figure from '../components/Figure';
 import { getTurn, getPhase, getPlayerFigures, getFiguresById, getActivePlayer,
  getAssignedAttacks,
@@ -9,27 +10,30 @@ import {
  selectAttackTarget as selectAttackTargetCreator,
   } from '../actionCreators';
 
-const FigureRow = ({ turn, phase, active, attackSource, selection, selectAttackSource, selectAttackTarget, figures }) => (
+const FigureRow = ({ turn, phase, active, attackSource, selection, selectAttackSource, selectAttackTarget, figures, upOrDown }) => (
   <div className="figure_row" style={{ display: 'flex' }}>
     {figures.map(f => {
       let handler = () => {};
-      let bulba = 'emptyhandler';
       if (turn > 0) handler = active ? selectAttackSource : selectAttackTarget;
-      if (turn > 0) bulba = active ? 'selectAttackSource' : 'selectAttackTarget';
       const sel = attackSource === f.id;
-      console.debug("f: %o", f); // FIXME: prepareFigures sometimes generates array with 1 undefined element
       return (
-        <div key={f.id}>
-          <span>{bulba}</span>
-          <Figure key={f.id} sel={sel} handler={handler} {...f} />
-          {f.attackers ? <span>Attackers:</span> : ''}
-          {f.attackers.map(a => <Figure key={a.id} sel={attackSource === a.id} handler={selectAttackSource} {...a} />)}
+        <div key={f.id} style={{ display: 'flex', 'flex-direction': 'column', 'justify-content': (upOrDown) ? 'flex-start' : 'flex-end' }}>
+          <div style={{ order: 2 }} >
+            <Figure key={f.id} sel={sel} handler={handler} {...f} />
+          </div>
+          <div style={{ display: 'flex', 'flex-direction': 'column', order: (upOrDown ? 3 : 1) }} >
+            <div style={{ order: (upOrDown ? 1 : 3) }} >
+              {(f.attackers.length === 0) ? '' : (upOrDown ? '↑' : '↓')}
+            </div>
+            <div style={{ order: 2 }}>
+              {f.attackers.map(a => <Figure key={a.id} sel={attackSource === a.id} handler={selectAttackSource} {...a} />)}
+            </div>
+          </div>
         </div>
         );
     })}
   </div>
   );
-
 
 const amIActivePlayer =
   (state, playerId) => getActivePlayer(state) === playerId;
@@ -42,13 +46,13 @@ const prepareFigures = (state, playerId) => {
 
   const sources = getAssignedAttacks(state);
 
-console.debug('active: %o, sources: %o', active, sources);
+  console.debug('active: %o, sources: %o', active, sources);
 
   return playerFigures
 /*         .filter(pf => (!active || !sources.includes(pf.id))) // remove attacking*/
          .filter(pf => {
-          console.debug("pf.id: %o, sources.includes(pf.id): %o",pf.id, sources.includes(pf.id));
-          return (!active || !sources.includes(pf.id));
+           console.debug('pf.id: %o, sources.includes(pf.id): %o', pf.id, sources.includes(pf.id));
+           return (!active || !sources.includes(String(pf.id)));
          })
          .map(f => (
            { ...f,
@@ -72,6 +76,7 @@ const mapStateToProps = (state, ownProps) => ({
   active: amIActivePlayer(state, ownProps.playerId),
   attackSource: getAttackSource(state),
   figures: prepareFigures(state, ownProps.playerId),
+  upOrDown: ownProps.playerId === 1,
 });
 
 export default connect(mapStateToProps, {
